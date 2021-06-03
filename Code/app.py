67174d1e3179023b661f654cf9d210567ef11170
Ben <benjybum2003@outlook.com>
2021-06-03 22:10:21 +0100

    Add to basket mechanism

--- Code/app.py
+++ Code/app.py
@@ -1,12 +1,35 @@
 # Import Flask
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 # Import SQLite, to interact with database
 import sqlite3
 # Import my algorithms file
 import algorithms
+# Import JSON library
+import json
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+def call_database(sqlstatement):
+    #Connect to sqlite
+    conn = sqlite3.connect('static\products.db')
+
+    #Create a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Send the SQL statement to the database
+    cursor.execute(sqlstatement)
+    
+    # Commit the changes to database
+    conn.commit()
+
+    # Retrieve all the products returned by the database
+    database_response = cursor.fetchall()
+
+    # Close connection
+    conn.close()
+
+    return database_response
 
 def get_products_brand(brand):
     # Convert capital letters to lowercase
@@ -113,16 +136,99 @@ def edit_product(formdata):
 
     # Close connection
     conn.close()
+
+def get_home_products():
+    # Open the file containing the product information for homepage
+    homedatafile = open("static\homedata.json", "r")
+    # Read its contents
+    homedata = homedatafile.read()
+
+    # Convert contents from JSON to python list
+    homedata = json.loads(homedata)
+    # Close file
+    homedatafile.close()
+
+    #Connect to sqlite
+    conn = sqlite3.connect('static\products.db')
+
+    #Create a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Generate an SQL statement to get products by their ID
+    sqlstatement = "SELECT * FROM productdata WHERE productid LIKE '" + homedata[0][0] + "'"
+
+    # For each additional product ID, append an extra condition on the SQL statement
+    for i in range(1, len(homedata[0])):
+        sqlstatement += " OR productid LIKE '" + homedata[0][i] + "'"
     
+    # Finish off the statement
+    sqlstatement += ";"
+
+    # Send the SQL statement to the database
+    cursor.execute(sqlstatement)
+    
+    # Commit the changes to database
+    conn.commit()
+
+    # Retrieve all the products returned by the database
+    products = cursor.fetchall()
+
+    # Close connection
+    conn.close()
+
+    # Save the extra information from the JSON
+    saleon = homedata[1]["saleon"]
+    saleimage = homedata[1]["saleimage"]
+
+    return products, saleon, saleimage
+
+def save_home_products():
+    data = [
+        [
+            "1009", 
+            "1007",
+            "1019",
+            "1013"
+        ],
+        {
+            "saleon": "true",
+            "saleimage": "static\\images\\nikesale.png"
+        }
+    ]
+
+    homedatafile = open("static\homedata.json", "w")
+    homedatafile.write(json.dumps(data, indent=4))
+    homedatafile.close()
+
+def load_basket_products(cookies):
+    product_id_list = []
+
+    # Append the productid of each cookie to a list
+    for productid in cookies:
+        product_id_list.append(productid)
+    print(product_id_list)
+
+    # Generate an SQL statement to get products by their ID
+    sqlstatement = "SELECT * FROM productdata WHERE productid LIKE '" + product_id_list[1] + "'"
+
+    # For each additional product ID, append an extra condition on the SQL statement
+    for i in range(1, len(product_id_list)):
+        sqlstatement += " OR productid LIKE '" + product_id_list[i] + "'"
+    
+    # Finish off the statement
+    sqlstatement += ";"
+
+    call_database
 
 
 @app.route('/home', methods =['GET', 'POST'])
 def home():
-    # Run the function to retrieve products matching a certain brand
-    products = get_products_brand('all')
+    save_home_products()
+
+    products, saleon, saleimage = get_home_products()
 
     # return website and data files
-    return render_template('index.html', rows=products)
+    return render_template('home.html', products=products, saleon=saleon, saleimage=saleimage)
 
 @app.route('/all', methods =['GET', 'POST'])
 def all():
@@ -188,14 +294,45 @@ def sort(page, sorttype):
     # Pass these products into the sorting algorithm
     products = algorithms.sort(sorttype, products)
 
-
     # return website and data files
     return render_template('index.html', rows=products)
 
 @app.route('/basket', methods =['GET'])
 def basket():
+    # Get all cookies stored in browser
+    cookies = request.cookies
+
+    # Pass all cookies into function to retrieve products from database
+    load_basket_products(cookies)
+
     # return website and data files
     return render_template('basket.html')
+
+# When product is added to basket
+@app.route('/basket/<productid>', methods=['POST'])
+def add_to_basket(productid):
+    # Save the product id and convert to string
+    productid = str(productid)
+    # Retrieve the quantity 
+    current_quantity = str(request.cookies.get(productid))
+
+    # If there is no cookie for current productid
+    if current_quantity == 'None':
+        resp = make_response(redirect(request.url))
+        # Create a cookie for current productid and set quantity to 1
+        resp.set_cookie(productid, '1')
+
+    # If there is already a cookie for current productid
+    else:
+        # Convert quantity to int and add 1
+        current_quantity = int(current_quantity)
+        current_quantity += 1
+
+        # Configure the response
+        resp = make_response(redirect(request.url))
+        resp.set_cookie(productid, str(current_quantity))
+
+    return resp
 
 @app.route('/admin', methods =['GET', 'POST'])
 def admin():
